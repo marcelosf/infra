1289b594c2c5a6c2e2ea250f45c91cd029ba825f
marcelosf <schneider.fei@gmail.com>
2017-09-21 14:34:13 +0000

    Atualização da factory SwitchPort issue#5

--- database/factories/SwitchPortFactory.php
+++ database/factories/SwitchPortFactory.php
@@ -8,6 +8,9 @@ $factory->define(\Infra\Model\Devices\SwitchPorts::class, function (Faker $faker
     $min_switch = DB::table('switches')->min('id');
     $max_switch = DB::table('switches')->max('id');
 
+    $max_patch = DB::table('ppanel')->max('id');
+    $min_patch = DB::table('ppanel')->min('id');
+
     return [
 
         'port' => $faker->randomNumber(2),
@@ -15,6 +18,8 @@ $factory->define(\Infra\Model\Devices\SwitchPorts::class, function (Faker $faker
         'poe_status' => $faker->boolean(50),
         'vlan' => $faker->randomNumber(2),
         'switch_id' => $faker->numberBetween($min_switch, $max_switch),
+        'ppanel_id' => $faker->unique()->numberBetween($min_patch, $max_patch)
+
 
     ];
 });
