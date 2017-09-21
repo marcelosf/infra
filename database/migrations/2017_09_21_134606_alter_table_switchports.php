c810a9fc3d4a6f0cc6bcc43383118fa920d41720
marcelosf <schneider.fei@gmail.com>
2017-09-21 13:58:10 +0000

    issue#2

---
+++ database/migrations/2017_09_21_134606_alter_table_switchports.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableSwitchports extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('switchports', function (Blueprint $table) {
+
+            $table->integer('ppanel_id')->unsigned();
+            $table->foreign('ppanel_id')->references('id')->on('ppanel');
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::table('switchports', function (Blueprint $table) {
+
+            $table->dropForeign('ppanel_id');
+
+        });
+
+    }
+}
